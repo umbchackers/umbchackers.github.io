95212c56c62885054c7fd1bf0c9e1abb22246c6e
Jason Owens <jao2@umbc.edu>
2018-07-20 16:17:31 -0400

    Finished app bar began splash section.

---
+++ js/temp.js
@@ -0,0 +1,136 @@
+function(t, e, n) {
+        "use strict";
+        Object.defineProperty(e, "__esModule", {
+            value: !0
+        });
+        var r = n(40)
+          , i = n(2)
+          , o = n(41)
+          , a = n(47)
+          , s = n(51)
+          , u = n(57)
+          , c = n(13)
+          , l = n(68)
+          , f = n(72)
+          , d = n(75)
+          , p = n(79)
+          , h = n(83)
+          , _ = n(14)
+          , v = n(88)
+          , y = n(90)
+          , b = n(15)
+          , g = n(95)
+          , m = n(3)
+          , E = n(99)
+          , C = n(4)
+          , O = n(101)
+          , T = n(103)
+          , k = n(106)
+          , I = n(113)
+          , A = n(119)
+          , w = n(122);
+        n.d(e, "autoInit", function() {
+            return r.a
+        }),
+        n.d(e, "base", function() {
+            return i
+        }),
+        n.d(e, "checkbox", function() {
+            return o
+        }),
+        n.d(e, "chips", function() {
+            return a
+        }),
+        n.d(e, "dialog", function() {
+            return s
+        }),
+        n.d(e, "drawer", function() {
+            return u
+        }),
+        n.d(e, "floatingLabel", function() {
+            return c
+        }),
+        n.d(e, "formField", function() {
+            return l
+        }),
+        n.d(e, "gridList", function() {
+            return f
+        }),
+        n.d(e, "iconButton", function() {
+            return d
+        }),
+        n.d(e, "iconToggle", function() {
+            return p
+        }),
+        n.d(e, "lineRipple", function() {
+            return _
+        }),
+        n.d(e, "linearProgress", function() {
+            return h
+        }),
+        n.d(e, "list", function() {
+            return v
+        }),
+        n.d(e, "menu", function() {
+            return y
+        }),
+        n.d(e, "notchedOutline", function() {
+            return b
+        }),
+        n.d(e, "radio", function() {
+            return g
+        }),
+        n.d(e, "ripple", function() {
+            return m
+        }),
+        n.d(e, "select", function() {
+            return E
+        }),
+        n.d(e, "selectionControl", function() {
+            return C
+        }),
+        n.d(e, "slider", function() {
+            return O
+        }),
+        n.d(e, "snackbar", function() {
+            return T
+        }),
+        n.d(e, "tabs", function() {
+            return k
+        }),
+        n.d(e, "textField", function() {
+            return I
+        }),
+        n.d(e, "toolbar", function() {
+            return A
+        }),
+        n.d(e, "topAppBar", function() {
+            return w
+        }),
+        r.a.register("MDCCheckbox", o.MDCCheckbox),
+        r.a.register("MDCChip", a.MDCChip),
+        r.a.register("MDCChipSet", a.MDCChipSet),
+        r.a.register("MDCDialog", s.MDCDialog),
+        r.a.register("MDCPersistentDrawer", u.MDCPersistentDrawer),
+        r.a.register("MDCTemporaryDrawer", u.MDCTemporaryDrawer),
+        r.a.register("MDCFloatingLabel", c.MDCFloatingLabel),
+        r.a.register("MDCFormField", l.MDCFormField),
+        r.a.register("MDCRipple", m.MDCRipple),
+        r.a.register("MDCGridList", f.MDCGridList),
+        r.a.register("MDCIconButtonToggle", d.MDCIconButtonToggle),
+        r.a.register("MDCIconToggle", p.MDCIconToggle),
+        r.a.register("MDCLineRipple", _.MDCLineRipple),
+        r.a.register("MDCLinearProgress", h.MDCLinearProgress),
+        r.a.register("MDCList", v.MDCList),
+        r.a.register("MDCNotchedOutline", b.MDCNotchedOutline),
+        r.a.register("MDCRadio", g.MDCRadio),
+        r.a.register("MDCSnackbar", T.MDCSnackbar),
+        r.a.register("MDCTab", k.MDCTab),
+        r.a.register("MDCTabBar", k.MDCTabBar),
+        r.a.register("MDCTextField", I.MDCTextField),
+        r.a.register("MDCMenu", y.MDCMenu),
+        r.a.register("MDCSelect", E.MDCSelect),
+        r.a.register("MDCSlider", O.MDCSlider),
+        r.a.register("MDCToolbar", A.MDCToolbar),
+        r.a.register("MDCTopAppBar", w.MDCTopAppBar)
+    }
