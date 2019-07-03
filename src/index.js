c046ececdbfb02b4f208d678200cfca5ce3d083e
TJ <ngo3@umbc.edu>
2019-07-03 18:38:21 -0400

    Social Media Update

--- src/index.js
+++ src/index.js
@@ -10,16 +10,17 @@ import Sponsor from './js/Sponsor';
 import Header from './js/Header';
 import Faq from './js/Faq';
 import Footer from './js/Footer';
+import SocialMedia from './js/Header'
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Navbar />, document.getElementById('myNav'));
 ReactDOM.render(<Header />, document.getElementById('top'));
+ReactDOM.render(<SocialMedia />, document.getElementById('top'));
 ReactDOM.render(<About />, document.getElementById('about'));
 ReactDOM.render(<Faq />, document.getElementById('faq'));
 ReactDOM.render(<Sponsor />, document.getElementById('sponsors'));
 ReactDOM.render(<Footer />, document.getElementById('footer'));
-// ReactDOM.render(<SocialMedia />, document.getElementById('social_media'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
