7c413b527f65d0b6296168d55ee5f002c0496298
Pranav <pranav.manik@gmail.com>
2019-08-28 22:49:25 -0400

    live site template set

--- assets/components/js/main.js
+++ assets/components/js/main.js
@@ -5,34 +5,33 @@ $(document).ready(function() {
   easterEgg.load("https://hackumbc.org/space-hackers");
 
 // Set the date we're counting down to
-// var countDownDate = new Date("Oct 21, 2018 11:30:00").getTime();
-// var countDownDate = new Date("Oct 21, 2018 13:00:00").getTime();
-// // Update the count down every 1 second
-// var x = setInterval(function() {
-//     // Get todays date and time
-//     var now = new Date().getTime(); 
-//     // Find the distance between now and the count down date
-//     var distance = countDownDate - now;
-//     // Time calculations for days, hours, minutes and seconds
-//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-//     // Output the result in an element with id="demo"
-//     if (days > 0){
-//       document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-//       + minutes + "m " + seconds + "s ";
-//     }
-//     else if (days == 0) {
-//       document.getElementById("demo").innerHTML = hours + "h "
-//       + minutes + "m " + seconds + "s ";
-//     }   
-//     // If the count down is over, write some text 
-//     if (distance < 0) {
-//         clearInterval(x);
-//         document.getElementById("demo").innerHTML = "Submit";
-//     }
-// }, 1000);
+var countDownDate = new Date("Aug 30, 2019 11:30:00").getTime();
+// Update the count down every 1 second
+var x = setInterval(function() {
+    // Get todays date and time
+    var now = new Date().getTime(); 
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Output the result in an element with id="demo"
+    if (days > 0){
+      document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+    }
+    else if (days == 0) {
+      document.getElementById("demo").innerHTML = hours + "h "
+      + minutes + "m " + seconds + "s ";
+    }   
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "Submit";
+    }
+}, 1000);
 
 //Hide Live button on mobile when clicked
 $("#live").on("click", function() {
