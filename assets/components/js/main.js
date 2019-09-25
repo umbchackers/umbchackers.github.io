866d02619e834817f6eb7adec35e6099670df530
Pranav <pranav.manik@gmail.com>
2019-09-25 05:52:12 -0400

    update time hacking begin

--- assets/components/js/main.js
+++ assets/components/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Sept 29, 2019 11:30:00").getTime();
+var countDownDate = new Date("Sept 29, 2019 10:30:00").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
     // Get todays date and time
