be5e12a373e75f112f4fe4dd637f11cd27363e22
thurst0612 <thurst1@umbc.edu>
2022-09-29 12:14:44 -0400

    Active Changes: - Added Schedule - Added Sponsors for this year - Updated Registration button to closing soon - FAQ Corrections
    Anticpated Changes:
    - Add Themes below Tracks
    - Add ILSB link in Navbar
    - Design Changes
    
    Potential Changes:
    - Meet the team page / sections
    - Links on track buttons to cover more detail
    - More suggestions welcome!

--- hackumbc-client/src/pages/Sponsors.js
+++ hackumbc-client/src/pages/Sponsors.js
@@ -28,34 +28,22 @@ const Sponsors = () => {
                     <p className='sponsor-us'>If you are interested in sponsoring us, please feel free to email us at sponsor@hackUMBC.org</p>
                 </div>
                 
-                <a href='https://www.standoutstickers.com/' target="_blank">
-                        <div className='sticker-logo' />
-                </a>
-                {/* <Rank title='DIAMOND'>
+                <Rank title='DIAMOND'>
                     <img src={require('../assets/sponsors/northrop.png')} alt='sponsor' className='logo-for-sponsor'/>
-                    <img src={require('../assets/sponsors/general-dynamics.png')} alt='sponsor' className='logo-for-sponsor'/>
                     <img src={require('../assets/sponsors/TRowe.png')} alt='sponsor' className='logo-for-sponsor'/>
                 </Rank>
                 <Rank title='GOLD'>
-                    <img src={require('../assets/sponsors/GEICO-Logo.png')} alt='sponsor' className='logo-for-sponsor'/>
                     <img src={require('../assets/sponsors/Booz-Allen-Hamiliton-logo.png')} alt='sponsor' className='logo-for-sponsor'/>
-                    <img src={require('../assets/sponsors/emochaLogo.png')} alt='sponsor' className='logo-for-sponsor'/>
+                    <img src={require('../assets/sponsors/apl_small_horizontal_black_1.png')} alt='sponsor' className='logo-for-sponsor'/>
                 </Rank>
                 <Rank title='SILVER'>
                     <img src={require('../assets/sponsors/cipher-tech.png')} alt='sponsor' className='logo-for-sponsor'/>
                     <img src={require('../assets/sponsors/alex_brown_text.jpg')} alt='sponsor' className='logo-for-sponsor'/>
-                    <img src={require('../assets/sponsors/clearedge-logo.png')} alt='sponsor' className='logo-for-sponsor'/>
-                    <img src={require('../assets/sponsors/Capital-One-Logo.png')} alt='sponsor' className='logo-for-sponsor'/>
+                    <img src={require('../assets/sponsors/textron_systems.png')} alt='sponsor' className='logo-for-sponsor'/>
                 </Rank>
-                <Rank title='BRONZE'>
-                    <img src={require('../assets/sponsors/cards-aganist-humanity.png')} alt='sponsor' className='logo-for-sponsor'/>
-                    <img src={require('../assets/sponsors/reverie-labs-logo-gray-p-500.png')} alt='sponsor' className='logo-for-sponsor'/>
-                    <img src={require('../assets/sponsors/wolfram-language.png')} alt='sponsor' className='logo-for-sponsor'/>
-                    <img src={require('../assets/sponsors/assemblyAI.png')} alt='sponsor' className='logo-for-sponsor'/>
-                    <img src={require('../assets/sponsors/smartlogic-logo-teal-1000.png')} alt='sponsor' className='logo-for-sponsor'/>
-                    <img src={require('../assets/sponsors/google-cloud-logo.png')} alt='sponsor' className='logo-for-sponsor'/>
-                    <img src={require('../assets/sponsors/sticker-mule.png')} alt='sponsor' className='logo-for-sponsor'/>
-                </Rank> */}
+                <a href='https://www.standoutstickers.com/' target="_blank">
+                        <div className='sticker-logo' />
+                </a>
             </div>
         </div>
     )
